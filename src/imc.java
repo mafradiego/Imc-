3e113265a0fcb577b185026f0ed721a7771a4839
mafradiego <mafradiego@hotmail.com>
2017-03-15 10:18:24 -0300

    Create imc.java

---
+++ src/imc.java
@@ -0,0 +1,41 @@
+IMC
+
+
+package calcularimc;
+
+import java.util.Scanner;
+
+public class Imc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner teclado = new Scanner(System.in);
+System.out.print("Digite a Altura");
+double Altura = teclado.nextDouble();
+System.out.print("Digite o Peso");
+double Peso = teclado.nextDouble();
+
+double imc = (Peso /(Altura*Altura));
+
+System.out.println("Imc= "+imc);
+
+if(imc<18.5) {
+	System.out.println ("Você está magro demais com esse Indice!");
+}
+else if (imc>=18.5 && imc<24.9) {
+		System.out.println("Você está normal com esse Indice!");
+}
+		
+else if (imc>=24.9 && imc<29.9){
+		System.out.println("Você está levemente Acima do peso com esse Indice!");
+}
+		
+else {
+  System.out.println("Você está com Sobre Peso Muito Acima do peso com esse Indice!");
+}
+	
+    
+
+   }
+}
+	
